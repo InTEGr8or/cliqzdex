d91d7ff49dbc25d5b34e7ea17d9e86fbb1a7d618
inTEGr8or <admin@thefullertonian.com>
2020-10-08 23:30:07 -0700

    clean up result mdown

--- quiz_generator/fill_services.py
+++ quiz_generator/fill_services.py
@@ -1,14 +1,14 @@
 import requests
 import yaml
 import os
-import json
 import lxml.html
-import lxml.etree as etree
-from lxml.cssselect import CSSSelector
 from markdownify import markdownify
+from tqdm import tqdm
+import re
 
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__)) + "/"
+print(ROOT_DIR)
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -21,9 +21,13 @@ class bcolors:
 
 def loop_file(file_name):
     out_services = []
+    omitted_services = []
+    re_2space = re.compile(r"\s{2,20}")
+    re_1stspace = re.compile(r"^\s|\s{2,20}|\s$")
+    re_colon = re.compile(r":")
     with open(file_name) as file:
         services = yaml.load(file, yaml.FullLoader)['services']
-        for service in services:
+        for service in tqdm(services):
             url = f"https://aws.amazon.com/{str.lower(service)}"
             response = requests.get(url).text
             tree = lxml.html.fromstring(response)
@@ -32,16 +36,24 @@ def loop_file(file_name):
             if(len(nodes) > 0):
                 node = nodes[0]
                 mdown = markdownify(lxml.html.tostring(node))
+                mdown = re_1stspace.sub("", mdown)
+                mdown = re_2space.sub(" ", mdown)
+                # mdown = re_colon.sub(";", mdown)
                 out_services.append({f"{service}": {
                     "url": url,
                     "description": mdown
                 } })
                 # print(f"{bcolors.OKGREEN}{service}{bcolors.ENDC}", mdown)
             else:
-                print(f"{bcolors.WARNING}{service}{bcolors.ENDC}")
+                # print(f"{bcolors.WARNING}{service}{bcolors.ENDC}")
+                omitted_services.append(service)
+
+    f = open(f"{ROOT_DIR}aws_omitted_services.yaml", "w")
+    f.write(yaml.dump(omitted_services, width=110))
+    f.close()
 
     f = open(f"{ROOT_DIR}aws_services_descriptions.yaml", "w")
-    f.write(yaml.dump(out_services))
+    f.write(yaml.dump(out_services, width=110))
     f.close()
 
 loop_file('D:/T3/Git/cliqz/cliqz/quizzes/aws_service_names.yaml')
